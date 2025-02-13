47af44886a168763a751c5962bd72462c3bb619c
Willow <willowc@haillow.com>
2025-02-12 22:51:30 -0500

    Updated student information in App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Studen ID: Put Your Student ID</h1>
-      <h1>Studen ID: Put Your Student Name</h1>
+      <h1>Student ID: 101540155</h1>
+      <h1>Student ID: Willow Campbell</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
-      Developerd By: Pritesh Patel
+      Developed By: Pritesh Patel
       <StudentList students={students} />
     </div>
   );
